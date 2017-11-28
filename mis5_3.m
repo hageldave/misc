416743e9fcd1194c749d528685bdd9a9df2476c0
haegeldd <haegele.david@gmail.com>
2017-11-28 17:28:14 +0100

    blah

--- mis5_3.m
+++ mis5_3.m
@@ -13,3 +13,15 @@ p = applysigmoid(X*beta);
 result = transpose(p-y)*X + 2*lam*transpose(beta);
 endfunction
 
+function result = gradient(X, y, lam, beta)
+p = applysigmoid(X*beta);
+result = transpose(X)*diag(p.*(1-p))*X + 2*eye(rows(X));
+endfunction
+
+n=5;
+d=3;
+X = randn(n,d)
+y = randi(2,n,1)-1
+lam = 0
+beta = rand(d,1)
+gradient(X,y,lam,beta)
