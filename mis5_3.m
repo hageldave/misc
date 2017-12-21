1143196f211cff8ff2ec258251121087e9dff5b7
David <david@THINKPADLNX>
2017-12-21 01:51:42 +0100

    tmfvc8.1

--- mis5_3.m
+++ mis5_3.m
@@ -13,9 +13,9 @@ p = applysigmoid(X*beta);
 result = transpose(p-y)*X + 2*lam*transpose(beta);
 endfunction
 
-function result = gradient(X, y, lam, beta)
+function result = hessian(X, y, lam, beta)
 p = applysigmoid(X*beta);
-result = transpose(X)*diag(p.*(1-p))*X + 2*eye(rows(X));
+result = transpose(X)*diag(p.*(1-p))*X + 2*eye(columns(X));
 endfunction
 
 n=5;
@@ -25,3 +25,4 @@ y = randi(2,n,1)-1
 lam = 0
 beta = rand(d,1)
 gradient(X,y,lam,beta)
+hessian(X,y,lam,beta)
